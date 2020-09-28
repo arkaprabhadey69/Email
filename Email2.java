6d51fc9c9c84e66280d928f19f7dfe1f472b1b34
arkaprabhadey <euphoricdey420@gmail.com>
2020-09-28 17:10:24 +0530

    [Arka]Added:Optional checks

--- Email2.java
+++ Email2.java
@@ -15,8 +15,8 @@ public class Email2 {
        
         while (true) {
 
-
-        	String x="^[a-zA-Z]+@[a-zA-Z]+.[(com)|(co)]+";
+              String x="^[a-zA-Z]+([._+-]{0,1}[a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[(com)|(co)|(net)]+(?:\\.[a-z]{2,}){0,1}$";
+        	
             Pattern pattern = Pattern.compile(x);
             //Scanner s2=new Scanner(System.in);
             String y=s.next();
