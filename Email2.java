77a94e21435ec2f4c1e97592f7505af62e23d933
arkaprabhadey <euphoricdey420@gmail.com>
2020-09-28 16:59:42 +0530

    [Arka]Added:Welcome Message

---
+++ Email2.java
@@ -0,0 +1,36 @@
+//package Regex;
+
+//package com.oops;
+
+import java.util.*;
+//import java.io.Console;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class Email2 {
+
+    public static void main(String[] args){
+    	final Scanner s=new Scanner(System.in);
+        System.out.println("Welcome to Email problem");
+       
+        while (true) {
+
+
+        	String x="^[a-zA-Z]+@[a-zA-Z]+.[(com)|(co)]+";
+            Pattern pattern = Pattern.compile(x);
+            //Scanner s2=new Scanner(System.in);
+            String y=s.next();
+
+            Matcher matcher =  pattern.matcher(y);
+
+            boolean found = false;
+            while (matcher.find()) {
+                System.out.println("Valid email");
+                found = true;
+            }
+            if(!found){
+                System.out.println("Invalid Email");
+            }
+        }
+    }
+}
